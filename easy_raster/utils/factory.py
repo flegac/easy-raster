e592ac79b74dcec4f37f842c3247e20b99a2c2bb
flegac <florent.legac@gmail.com>
2024-09-03 10:41:47 +0200

    feat: add seed parameter to gradientNoise generation

--- easy_raster/utils/factory.py
+++ easy_raster/utils/factory.py
@@ -7,7 +7,9 @@ from easy_raster.transform.normalizer import Normalizer
 class RFactory:
 
     @staticmethod
-    def heightmap(size: int, n: int = 5):
+    def heightmap(size: int, n: int = 5, seed: int = None):
+        if seed is not None:
+            np.random.seed(seed)
         height = np.zeros((size, size))
         weight = 0
         for i in range(n):
@@ -23,7 +25,9 @@ class RFactory:
         return Normalizer.normalize(height)
 
     @staticmethod
-    def random_gradient(shape: tuple[int, ...]):
+    def random_gradient(shape: tuple[int, ...], seed: int = None):
+        if seed is not None:
+            np.random.seed(seed)
         grad = np.exp(2j * np.pi * np.random.rand(*shape))
         grad = np.dstack([grad.real, grad.imag])
         return np.moveaxis(grad, 2, 0)
